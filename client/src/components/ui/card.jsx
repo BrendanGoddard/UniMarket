b51a87efdc11130e98b3282f943bf5e9a109a8b1
Evan <evankwak19@gmail.com>
2025-09-29 22:22:54 -0400

    replaced client with main page

--- client/src/components/ui/card.jsx
+++
@@ -1,8 +0,0 @@
-export function Card({ children, className = '' }) {
-return <div className={`card ${className}`}>{children}</div>;
-}
-
-
-export function CardContent({ children }) {
-return <div className="flex flex-col gap-2">{children}</div>;
-}
